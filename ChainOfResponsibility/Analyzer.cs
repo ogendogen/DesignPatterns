a27ce3eb8862dea87a0e7b6634e968c222d4170e
ogendogen <kosmos500@interia.pl>
2019-10-24 22:46:36 +0200

    Analyzer created

---
+++ ChainOfResponsibility/Analyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility
+{
+    public class Analyzer : IBank
+    {
+        private IBank _nextStage;
+        public void Process(CreditRequest creditRequest, ref string errorMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetNext(IBank nextStage)
+        {
+            _nextStage = nextStage;
+        }
+    }
+}
