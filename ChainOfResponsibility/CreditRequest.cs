700b4e711ed1d0d0681a3db6f058efc3a1ec52a9
ogendogen <kosmos500@interia.pl>
2019-10-24 19:59:49 +0200

    Added credit history

---
+++ ChainOfResponsibility/CreditRequest.cs
@@ -0,0 +1,12 @@
+﻿using ChainOfResponsibility.Types;
+
+namespace ChainOfResponsibility
+{
+    public class CreditRequest
+    {
+        public double Amount { get; set; }
+        public double Percentage { get; set; }
+        public Person Requester { get; set; }
+        public CreditHistory CreditHistory { get; set; }
+    }
+}
---
+++ ChainOfResponsibility/Types/CreditHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility.Types
+{
+    public enum CreditHistory
+    {
+        Excellent,
+        Good,
+        Average,
+        Bad
+    }
+}
