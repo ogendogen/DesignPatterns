dfc6d5604a1280b0c215da8d3d7ef411f57faec1
ogendogen <kosmos500@interia.pl>
2019-10-24 20:12:23 +0200

    Person extended

--- ChainOfResponsibility/Person.cs
+++ ChainOfResponsibility/Person.cs
@@ -5,6 +5,8 @@ namespace ChainOfResponsibility
     public class Person
     {
         public string Name { get; set; }
+        public string LastProp { get; set; }
+        public int Age { get; set; }
         public double MonthIncome { get; set; }
         public Employment EmploymentType { get; set; }
     }
