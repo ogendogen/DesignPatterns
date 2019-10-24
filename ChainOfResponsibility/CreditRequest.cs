b9746282a14b29c47150c32f918030aaefd1fd9b
ogendogen <kosmos500@interia.pl>
2019-10-24 20:12:42 +0200

    CreditRequest extended

--- ChainOfResponsibility/CreditRequest.cs
+++ ChainOfResponsibility/CreditRequest.cs
@@ -8,5 +8,6 @@ namespace ChainOfResponsibility
         public double Percentage { get; set; }
         public Person Requester { get; set; }
         public CreditHistory CreditHistory { get; set; }
+        public bool HasAlreadyCredit { get; set; }
     }
 }
