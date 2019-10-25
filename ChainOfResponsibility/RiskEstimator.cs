02730291c011d362ed8bb0f8e48562d7197c727b
ogendogen <kosmos500@interia.pl>
2019-10-25 21:45:19 +0200

    RiskEstimator finished

--- ChainOfResponsibility/RiskEstimator.cs
+++ ChainOfResponsibility/RiskEstimator.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ChainOfResponsibility.Types;
 
 namespace ChainOfResponsibility
 {
@@ -13,7 +11,12 @@ namespace ChainOfResponsibility
             {
                 returnMessage = "Credit acquired";
             }
-            else if (_nextStage != null)
+            else
+            {
+                returnMessage = "RiskEstimator failed";
+            }
+            
+            if (_nextStage != null)
             {
                 _nextStage.Process(creditRequest, ref returnMessage);
             }
@@ -21,7 +24,29 @@ namespace ChainOfResponsibility
 
         private bool IsRiskEstimatedOk(CreditRequest creditRequest)
         {
-            throw new NotImplementedException();
+            switch(creditRequest.RiskOfInvestment)
+            {
+                case RiskLevel.Low:
+                    {
+                        return creditRequest.Amount <= 500000 && creditRequest.Percentage >= 15.0 && creditRequest.TimeInMonths >= 36;
+                    }
+                case RiskLevel.Normal:
+                    {
+                        return creditRequest.Amount <= 250000 && creditRequest.Percentage >= 12.0 && creditRequest.TimeInMonths >= 24;
+                    }
+                case RiskLevel.High:
+                    {
+                        return creditRequest.Amount <= 100000 && creditRequest.Percentage >= 10.0 && creditRequest.TimeInMonths >= 24;
+                    }
+                case RiskLevel.VeryHigh:
+                    {
+                        return creditRequest.Amount <= 75000 && creditRequest.Percentage >= 8.0 && creditRequest.TimeInMonths >= 12;
+                    }
+                default:
+                    {
+                        return false;
+                    }
+            }
         }
 
         public void SetNext(IBank nextStage)
