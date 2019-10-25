140386aec0ca602ff162b099816e72a42508cfec
ogendogen <kosmos500@interia.pl>
2019-10-25 17:13:46 +0200

    Implementation of Analyzer

--- ChainOfResponsibility/Analyzer.cs
+++ ChainOfResponsibility/Analyzer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using ChainOfResponsibility.Types;
 
 namespace ChainOfResponsibility
 {
@@ -26,7 +25,62 @@ namespace ChainOfResponsibility
 
         public bool IsAnalysisFine(CreditRequest creditRequest)
         {
-            throw new NotImplementedException();
+            int points = 0;
+            points += GetPointsForEmployment(creditRequest.Requester.EmploymentType);
+            points += GetPointsForIncome(creditRequest.Requester.MonthIncome);
+            points += GetPointsForAge(creditRequest.Requester.Age);
+            points += GetPointsForCreditHistory(creditRequest.Requester.CreditHistory);
+            points += GetPointsForCreditStatus(creditRequest.Requester.HasAlreadyCredit);
+            
+            return points > 8;
+        }
+
+        private int GetPointsForCreditStatus(bool hasAlreadyCredit)
+        {
+            return hasAlreadyCredit ? 1 : 0;
+        }
+
+        private int GetPointsForEmployment(Employment employmentType)
+        {
+            return (int)employmentType;
+        }
+        
+        private int GetPointsForIncome(double monthIncome)
+        {
+            if (monthIncome < 2000)
+            {
+                return 0;
+            }
+            if (monthIncome >= 2000 && monthIncome < 4000) 
+            {
+                return 1;
+            }
+            else if (monthIncome >= 4000 && monthIncome < 6000)
+            {
+                return 2;
+            }
+            else if (monthIncome >= 6000 && monthIncome < 8000)
+            {
+                return 3;
+            }
+            else if (monthIncome > 8000 && monthIncome < 10000)
+            {
+                return 4;
+            }
+            else
+            {
+                return 5;
+            }
+        }
+        
+        private int GetPointsForAge(int age)
+        {
+            return age > 35 ? 1 : 0;
+        }
+
+        private int GetPointsForCreditHistory(CreditHistory creditHistory)
+        {
+            return (int)creditHistory;
         }
     }
 }
