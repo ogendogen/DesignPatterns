bb67bf8050d661c5d1f6b45a33053fb5bd456c4d
ogendogen <kosmos500@interia.pl>
2019-10-25 17:40:24 +0200

    Added RiskEstimator

---
+++ ChainOfResponsibility/RiskEstimator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility
+{
+    public class RiskEstimator : IBank
+    {
+        private IBank _nextStage;
+        public void Process(CreditRequest creditRequest, ref string returnMessage)
+        {
+            if (IsRiskEstimatedOk(creditRequest))
+            {
+                returnMessage = "Credit acquired";
+            }
+            else if (_nextStage != null)
+            {
+                _nextStage.Process(creditRequest, ref returnMessage);
+            }
+        }
+
+        private bool IsRiskEstimatedOk(CreditRequest creditRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetNext(IBank nextStage)
+        {
+            _nextStage = nextStage;
+        }
+    }
+}
