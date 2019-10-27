b8dea560ac576d70c3bb59750607166451511f67
ogendogen <kosmos500@interia.pl>
2019-10-27 11:41:20 +0100

    Added abstract builder (ICoffeeBuilder)

---
+++ Builder/ICoffeeBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    // Abstract Builder
+    interface ICoffeeBuilder
+    {
+        void ChooseEspresso();
+        void ChooseLatte();
+        void ChooseCappuccino();
+
+        void ChooseBig();
+        void ChooseSmall();
+
+        void PutNoSugar();
+        void PutOneSpoonOfSugar();
+        void PutTwoSpoonsOfSugar();
+
+        void AddDoubleCoffee();
+        void AddCinamon();
+        void AddExtraMilk();
+
+        Coffee GetCoffee();
+    }
+}
