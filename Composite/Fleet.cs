128f57114dfab43a2a2b128711e6a3675152f9f3
ogendogen <kosmos500@interia.pl>
2019-10-27 13:52:18 +0100

    Added IVehicle inheritance to Fleet

--- Composite/Fleet.cs
+++ Composite/Fleet.cs
@@ -2,7 +2,21 @@
 
 namespace Composite
 {
-    public class Fleet
+    public class Fleet : IVehicle
     {
+        public void GoBackToFleet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GoForClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StandBy()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
