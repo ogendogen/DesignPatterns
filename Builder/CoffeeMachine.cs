df47bc37353c3b4315590139d022379204a45402
ogendogen <kosmos500@interia.pl>
2019-10-27 11:42:02 +0100

    Added director (CoffeeMachine)

---
+++ Builder/CoffeeMachine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    // Director
+    public class CoffeeMachine
+    {
+        private ICoffeeBuilder _coffeeBuilder;
+
+        public CoffeeMachine()
+        {
+            _coffeeBuilder = new CoffeeBuilder();
+        }
+
+        public Coffee GetBigEspressoWithTwoSpoonsAndCinamon()
+        {
+            _coffeeBuilder.ChooseEspresso();
+            _coffeeBuilder.ChooseBig();
+            _coffeeBuilder.PutTwoSpoonsOfSugar();
+            _coffeeBuilder.AddCinamon();
+
+            return _coffeeBuilder.GetCoffee();
+        }
+
+        public Coffee GetSmallLatteWithNoSugarExtraMilkAndDoubleCoffee()
+        {
+            _coffeeBuilder.ChooseLatte();
+            _coffeeBuilder.ChooseSmall();
+            _coffeeBuilder.PutNoSugar();
+            _coffeeBuilder.AddExtraMilk();
+            _coffeeBuilder.AddDoubleCoffee();
+
+            return _coffeeBuilder.GetCoffee();
+        }
+
+        public Coffee GetBigCappuccinoWithOneSpoon()
+        {
+            _coffeeBuilder.ChooseCappuccino();
+            _coffeeBuilder.ChooseBig();
+            _coffeeBuilder.PutOneSpoonOfSugar();
+
+            return _coffeeBuilder.GetCoffee();
+        }
+    }
+}
