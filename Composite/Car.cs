d82ebeca1af0ccf11fba4eca8546a0196e669f2c
ogendogen <kosmos500@interia.pl>
2019-10-27 13:19:05 +0100

    Added leaves (Car, Motorbike, VAN)

---
+++ Composite/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    public class Car : IVehicle
+    {
+        public int ID { get; set; }
+
+        public void GoBackToFleet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GoForClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StandBy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Composite/Motorbike.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    public class Motorbike : IVehicle
+    {
+        public int ID { get; set; }
+
+        public void GoBackToFleet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GoForClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StandBy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
